a7efbc9d3697d3f09d3945a72ececd8accd92f1d
Genia Kazymova <genia@trln.org>
2019-12-09 14:50:03 +0000

    Add test for Quran - working version

--- spec/punctuation/quote_spec.rb
+++ spec/punctuation/quote_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
-describe 'smart single quotes' do  
+describe 'single quotes' do
   context 'Encyclopaedia of the Qurʼān' do
-    it 'Qurʼan and Qur\'an should return the same results' do
-      resp = solr_resp_ids_from_query 'Encyclopaedia of the Qurʼān'
+    it 'the query should succeed if it contains a smart quote' do
+      resp = solr_resp_ids_titles(all_fields_search_duke('Encyclopaedia of the Qurʼan'))
       expect(resp).to include('DUKE003888510')
-    end
+    end    
 
-    it 'Qurʼan and Qur\'an should return the same results' do
-      resp = solr_resp_ids_from_query 'Encyclopaedia of  the Qur\'an'
+    it 'the query should succeed if it contains a straight quote' do
+      resp = solr_resp_ids_titles(all_fields_search_duke('Encyclopaedia of the Qur\'an'))
+      expect(resp).to include('DUKE003888510')
+    end    
+
+    it 'the query should succeed if it does not contain a quote' do
+      resp = solr_resp_ids_titles(all_fields_search_duke('Encyclopaedia of the Quran'))
       expect(resp).to include('DUKE003888510')
     end
   end
-end  
+end
